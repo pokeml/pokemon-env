649b7ff5a378cd8a559ddff220d21b25816e99e3
Christopher Wolff <christopher.wolff@duke.edu>
2018-07-15 00:58:50 -0700

    Test gen7ou battles with custom teams

--- main.js
+++ main.js
@@ -7,19 +7,20 @@
 const BattleStreams = require('./Pokemon-Showdown/sim/battle-stream');
 const Dex = require('./Pokemon-Showdown/sim/dex');
 const RandomPlayerAI = require('./agents/random-agent.js')
+const teams = require('./data/teams')
 
 const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream());
 
 const spec = {
-	formatid: "gen7customgame",
+	formatid: "gen7ou",
 };
 const p1spec = {
 	name: "Bot 1",
-	team: Dex.packTeam(Dex.generateTeam('gen7randombattle')),
+	team: teams['gen7ou'][0],
 };
 const p2spec = {
 	name: "Bot 2",
-	team: Dex.packTeam(Dex.generateTeam('gen7randombattle')),
+	team: teams['gen7ou'][1],
 };
 
 const p1 = new RandomPlayerAI(streams.p1);
