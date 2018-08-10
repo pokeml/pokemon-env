edf9be94300bc34707dfb6ae22aa8f435e85af60
Christopher Wolff <christopher.wolff@duke.edu>
2018-08-09 21:12:27 -0700

    Fix eslint errors

--- tests/state-representation/test-state-representation.test.js
+++ tests/state-representation/test-state-representation.test.js
@@ -2,9 +2,12 @@ const representation = require('../../src/state-representation/state-representat
 
 
 test('test one hot encoding', () => {
-    expect(representation.getOneHotEncoding(3, 5)).toEqual([0, 0, 0, 1, 0]);
+    expect(representation.createOneHotEncoding(3, 5)).toEqual([0, 0, 0, 1, 0]);
 });
 
 test('test one hot encoding', () => {
-    expect(representation.oneHotEncodeAbility("contrary")).toEqual([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    expect(representation.encodeAbility('contrary')).toEqual(
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    );
 });
