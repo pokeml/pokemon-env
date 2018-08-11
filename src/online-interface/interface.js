c64950d61146ffc5cf784de752f92b2cef7f1bc7
Christopher Wolff <christopher.wolff@duke.edu>
2018-08-11 16:08:23 -0700

    Add interface class

---
+++ src/online-interface/interface.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * An interface that enables communication with the Pokémon Showdown server.
+ */
+class Interface {
+    /**
+     * Create a new interface.
+     */
+    constructor() {}
+}
+
+module.exports = Interface;
