6fb55959461fd35acfe4f9ee946ec7abe8aa9c36
mikail <myay@github.com>
2018-08-12 03:57:26 +0200

    agent identifies its own and the opponent's active pokemon

--- scripts/run-offline-sim.js
+++ scripts/run-offline-sim.js
@@ -5,10 +5,10 @@
 'use strict';
 
 const BattleStreams = require('../Pokemon-Showdown/sim/battle-stream');
-const RandomAgent = require('../src/agents/random-agent');
-const TestAgent = require('../src/agents/test-agent');
-const ChecksSwitchAgent = require('../src/agents/checksswitch-agent')
-const teams = require('../data/teams');
+// const RandomAgent = require('../src/agents/random-agent');
+// const TestAgent = require('../src/agents/test-agent');
+const ChecksSwitchAgent = require('../src/agents/checksswitch-agent');
+// const teams = require('../data/teams');
 
 // get 2v2 team
 const teams2v2 = require('../data/teams2v2');
@@ -18,19 +18,21 @@ const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream())
 const spec = {
     formatid: 'gen7ou',
 };
+// offense bot
 const p1spec = {
     name: 'Bot 1',
     team: teams2v2['gen7ou'][0],
 };
+// stall bot
 const p2spec = {
     name: 'Bot 2',
     team: teams2v2['gen7ou'][1],
 };
 
-console.log(p1spec)
-console.log(p2spec)
+console.log(p1spec);
+console.log(p2spec);
 // eslint-disable-next-line no-unused-vars
-const p1 = new TestAgent(streams.p1, true);
+const p1 = new ChecksSwitchAgent(streams.p1, true);
 // eslint-disable-next-line no-unused-vars
 const p2 = new ChecksSwitchAgent(streams.p2);
 
--- src/agents/checksswitch-agent.js
+++ src/agents/checksswitch-agent.js
@@ -2,7 +2,7 @@
 
 const BattleAgent = require('./base-agent');
 const _ = require('underscore');
-var checks = require('../../data/checks.json')
+const checks = require('../../data/checks.json');
 
 /**
  * An agent that chooses actions based on checks.json
@@ -17,9 +17,55 @@ class ChecksSwitchAgent extends BattleAgent {
      * @return {string}
      */
     act(battle, actions, info) {
+        // determine player and opponent
+        const player = info.side.id;
+        let opponent = '';
+
+        if (player === 'p1') {
+            opponent = 'p2';
+        } else {
+            opponent = 'p1';
+        }
+        console.log(`Players: ${player}, ${opponent}`);
+
+        // get my active mon
+        let myActiveMon;
+        let myMons = info.side.pokemon;
+        for (const pokemon of myMons) {
+            if (pokemon.active == true) {
+                myActiveMon = pokemon.ident.slice(4);
+            }
+        }
+        // console.log(myActiveMon);
+
+        // get the opponent's active mon
+        let oppActiveMon;
+        // if p1, go into Bot 2's pokemon list to search for the active pokemon
+        if (player === 'p1') {
+            if (battle.sides[1].active[0]) {
+                oppActiveMon = battle.sides[1].active[0].species;
+            } else {
+                oppActiveMon = 'Missingno';
+                // console.log('Not initialized yet');
+            }
+        } else {
+            if (battle.sides[0].active[0]) {
+                oppActiveMon = battle.sides[0].active[0].species;
+            } else {
+                oppActiveMon = 'Missingno';
+                // console.log('Not initialized yet');
+            }
+        }
+        // console.log(oppActiveMon);
+        console.log(`My active: ${myActiveMon}, opponent's active: ${oppActiveMon}`);
+
+        // find out from the checks graph how well you deal with it
+
+        // try to find a pokemon on your team that does better against the current opposing pokemon. if the current pokemon is already the best choice, attack. if there is a pokemon that does better against the current opposing pokemon, switch to it
+
         const action = _.sample(actions);
-        console.log(`>>CS Agent: ${action}`);
-        console.log(checks['keldeo']);
+        // console.log(`>>CS Agent: ${action}`);
+        // console.log(checks['keldeo']);
         return action;
     }
 }
