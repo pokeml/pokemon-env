2e265c78603a05e8263c4d14fa033a3b4e3423b7
mikail <myayla1245@gmail.com>
2018-08-14 01:33:35 +0200

    search gsi if agent has ssi as active to the opposing active

--- src/agents/checksswitch-agent.js
+++ src/agents/checksswitch-agent.js
@@ -42,6 +42,8 @@ class ChecksSwitchAgent extends BattleAgent {
 
         // get my active mon
         let myActiveMon;
+        let oppActiveMon;
+
         let myMons = info.side.pokemon;
         for (const pokemon of myMons) {
             if (pokemon.active == true) {
@@ -51,7 +53,6 @@ class ChecksSwitchAgent extends BattleAgent {
         // console.log(myActiveMon);
 
         // get the opponent's active mon
-        let oppActiveMon;
         // if p1, go into Bot 2's pokemon list to search for the active pokemon
         if (player === 'p1') {
             if (battle.sides[1].active[0]) {
@@ -90,8 +91,8 @@ class ChecksSwitchAgent extends BattleAgent {
             }
         }
         // console.log(`>> ${player}: I'm ${player}, my opponent is ${opponent}.`);
-        console.log(`>> ${player}: My active ${myActiveMon} is ${typeOfCheck}`);
-        console.log(`>> ${player}: to my opponent's active ${oppActiveMon}`);
+        console.log(`>> ${player}: My active ${myActiveMon} is ${typeOfCheck} `
+          + `to my opponent's active ${oppActiveMon}`);
 
         let action;
         // if it's the first turn (team preview), choose lead and return
@@ -107,7 +108,6 @@ class ChecksSwitchAgent extends BattleAgent {
 
         // random action if below fails
         action = _.sample(actions);
-        // console.log(actions);
         let stayInActions;
         switch (typeOfCheck) {
         case 'gsi':
@@ -124,9 +124,9 @@ class ChecksSwitchAgent extends BattleAgent {
             if (moveIsPossible) {
                 // console.log(actions);
                 // console.log(`>> ${player}: ${stayInActions}`);
-                // TODO: chose optimal move
                 stayInActions = actions.filter((e) => (e.type === 'switch'));
                 // console.log(stayInActions);
+                // TODO: 1v1
                 action = _.sample(stayInActions);
             } else {
                 // TODO: elaborate situation in which switching is only move, and use a stratgy
@@ -135,11 +135,59 @@ class ChecksSwitchAgent extends BattleAgent {
 
             break;
         case 'ssi':
-            console.log(`>> ${player}: Search gsi.`);
-            // if gsi empty, stay in
+            console.log(`>> ${player}: Search gsi`);
+            // check if switch action is possible
+            let switchIsPossible = false;
+            for (const acts of actions) {
+                if (acts.type === 'switch') {
+                    switchIsPossible = true;
+                    break;
+                }
+            }
+            if (switchIsPossible) {
+                // if we are in this case, we already know our active pokemon is not gsi
+                let currentPokemon;
+                let gsiExists = false;
+                // the gsi pokemon's index and name (to which we should switch)
+                let gsiIndex;
+                let gsiName;
+                // check if gsi to the opponent's active exists in own team
+                for (const pokemon of info.side.pokemon) {
+                    // we want to switch, so skip the own active pokemon
+                    if (!pokemon.active) {
+                        // get list of own pokemon, and store the first pokemon
+                        currentPokemon = pokemon.ident.slice(4).toLowerCase().replace(/\s/g, '');
+                        // check if currentPokemon appears in oppActiveMon's gsi list
+                        if (oppMonChecks) {
+                            gsiIndex = oppMonChecks.gsi.indexOf(`${currentPokemon}`);
+                            if (gsiIndex > -1) {
+                                gsiExists = true;
+                                gsiName = oppMonChecks.gsi[gsiIndex];
+                                // found gsi, exit loop
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (gsiExists) {
+                    console.log(`>> ${player}: Found gsi, switch to ${gsiName}`);
+                    // reset values
+                    gsiIndex = -1;
+                    gsiExists = false;
+                } else {
+                    console.log(`>> ${player}: No gsi found, stay in`);
+                    stayInActions = actions.filter((e) => (e.type === 'switch'));
+                    // TODO: 1v1
+                    action = _.sample(stayInActions);
+                }
+            } else {
+                // switching is not a valid choice
+                // TODO: 1v1
+                console.log(`>> ${player}: Switching not possible, stay in`);
+            }
             break;
         case 'nsi':
-            console.log(`>> ${player}: Search gsi/ssi.`);
+            console.log(`>> ${player}: Search gsi/ssi`);
             // if gsi/ssi empty, stay in
             break;
         case '--':
