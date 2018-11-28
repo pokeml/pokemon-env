aa4d48ee623feb74c7ccb129d60e96b70658aa2f
Christopher Wolff <cfw13@duke.edu>
2018-11-28 18:13:20 -0500

    Rename vars

--- examples/scripts/run-offline-sim.js
+++ examples/scripts/run-offline-sim.js
@@ -5,7 +5,7 @@ const RandomAgent = require('../agents/random-agent');
 const teams = require('../../data/teams');
 
 // Parameters
-const numEpisodes = 10;
+const numEpisodes = 100;
 const maxSteps = 1000;
 
 // Player 1 specs
@@ -13,14 +13,14 @@ const p1 = {
     name: 'Player 1',
     team: teams['gen7ou'][0],
 };
-let p1Agent = new RandomAgent();
+let agent1 = new RandomAgent();
 
 // Player 2 specs
 const p2 = {
     name: 'Player 2',
     team: teams['gen7ou'][1],
 };
-let p2Agent = new RandomAgent();
+let agent2 = new RandomAgent();
 
 // Init environment
 let env = new Environment('gen7ou', p1, p2);
@@ -28,14 +28,14 @@ let env = new Environment('gen7ou', p1, p2);
 // Main loop
 for (let episode = 1; episode <= numEpisodes; episode++) {
     console.log(`Episode ${episode}`);
-    var {p1State, p2State} = env.reset();
+    var {state1, state2} = env.reset();
     for (let t = 1; t <= maxSteps; t++) {
         // Choose actions
-        let p1Action = p1Agent.act(p1State, env.getActionSpace('p1'));
-        let p2Action = p2Agent.act(p2State, env.getActionSpace('p2'));
+        let action1 = agent1.act(state1, env.getActionSpace('p1'));
+        let action2 = agent2.act(state2, env.getActionSpace('p2'));
 
         // Advance environment
-        var {p1State, p2State, done, info} = env.step(p1Action, p2Action);
+        var {state1, state2, done, info} = env.step(action1, action2);
 
         if (done) {
             console.log(`Winner: ${info.winner}`);
