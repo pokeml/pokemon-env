6c8d9cb6ec8855277f0cf3cab08b254989e15209
Christopher Wolff <cfw13@duke.edu>
2018-11-28 20:49:48 -0500

    Improve code quality

--- examples/scripts/sim.js
+++ examples/scripts/sim.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const Environment = require('../../src/env');
+const Environment = require('pokemon-env');
 const RandomAgent = require('../agents/random-agent');
+
 const teams = require('../../data/teams');
 
 // Parameters
-const numEpisodes = 100;
+const numEpisodes = 10;
 const maxSteps = 1000;
 
 // Player 1 specs
@@ -13,17 +14,17 @@ const p1 = {
     name: 'Player 1',
     team: teams['gen7ou'][0],
 };
-let agent1 = new RandomAgent();
+const agent1 = new RandomAgent();
 
 // Player 2 specs
 const p2 = {
     name: 'Player 2',
     team: teams['gen7ou'][1],
 };
-let agent2 = new RandomAgent();
+const agent2 = new RandomAgent();
 
 // Init environment
-let env = new Environment('gen7ou', p1, p2);
+const env = new Environment('gen7randombattle', p1, p2);
 
 // Main loop
 for (let episode = 1; episode <= numEpisodes; episode++) {
@@ -31,8 +32,8 @@ for (let episode = 1; episode <= numEpisodes; episode++) {
     var {state1, state2} = env.reset();
     for (let t = 1; t <= maxSteps; t++) {
         // Choose actions
-        let action1 = agent1.act(state1, env.getActionSpace('p1'));
-        let action2 = agent2.act(state2, env.getActionSpace('p2'));
+        const action1 = agent1.act(state1, env.getActionSpace('p1'));
+        const action2 = agent2.act(state2, env.getActionSpace('p2'));
 
         // Advance environment
         var {state1, state2, done, info} = env.step(action1, action2);
