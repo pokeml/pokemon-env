174c80788a04850655ea097ccaabad55dc730a39
Christopher Wolff <cfw13@duke.edu>
2018-11-28 23:50:50 -0500

    Enable seeding for random agent

--- examples/agents/random-agent.js
+++ examples/agents/random-agent.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const Agent = require('../../src/agent');
-const _ = require('underscore');
+const seedrandom = require('seedrandom');
 
 /**
  * An agent that chooses actions uniformly at random.
  */
 class RandomAgent extends Agent {
+    /**
+     * @param {number} [seed]
+     */
+    constructor(seed = null) {
+        super();
+        this.seed = seed;
+        this.random = seedrandom(seed);
+    }
+
     /**
      * Choose an action.
      *
@@ -15,7 +24,17 @@ class RandomAgent extends Agent {
      * @return {string}
      */
     act(state, actions) {
-        return _.sample(actions);
+        return this._sample(actions);
+    }
+
+    /**
+     * Sample a random element from an array.
+     *
+     * @param {Array} arr
+     * @return {Object}
+     */
+    _sample(arr) {
+        return arr[Math.floor(this.random() * arr.length)];
     }
 }
 
